822c2ae79724813c19ff02c67916f343dfd1a4a8
Joel Kasisi <joelkasisi10@gmail.com>
2023-01-17 00:30:21 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    title: 'iPhone 14 Pro Max',
+    price: 1099.00,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkYDyn-GUTBvv3KrrSnRlnxbH15oEh4x5QHvMWLIIkRQ7v857AwtzCfpGZsZOT9-Nc_x4&usqp=CAU',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'iPhone 14 Pro',
+    price: 999.00,
+    img: 'https://my-apple-store.ru/wa-data/public/shop/products/74/26/12674/images/19734/19734.970.jpg',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'iPhone 14 Plus',
+    price: 899.00,
+    img: 'https://www.starhub.com/content/dam/catalog/starhub-store-iphone-14-plus-blue-front.png',
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: 'iPhone 14',
+    price: 799.00,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0iedPA_PJwtAXqrK1I1qHAEGFbVMMxTnECg&usqp=CAU',
+    amount: 1,
+  },
+];
